428ec41d093870f12ddc04d2846749cd2015b654
thilko <thilko.richter@googlemail.com>
2017-06-26 06:40:50 +0200

    work on terraform config

--- main.tf
+++ main.tf
@@ -1,8 +1,23 @@
 provider "aws" {
-  region     = "eu-central-1c"
+  region     = "${var.region}"
 }
 
 resource "aws_instance" "hourly" {
-  ami           = "ami-1c45e273"
+  ami           = "ami-8da700e2"
   instance_type = "t2.micro"
+  key_name = "deployer-key"
 }
+
+resource "aws_eip" "ip" {
+  instance = "${aws_instance.hourly.id}"
+}
+
+output "ip" {
+  value = "${aws_eip.ip.public_ip}"
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = ""
+}
+
